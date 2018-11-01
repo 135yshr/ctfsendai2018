488a085fdeffe7c402df1cfd359ccbbc3ed48c83
135yshr <isago@praisefirst.jp>
2018-11-02 02:54:22 +0900

    Initial commit

---
+++ user.go
@@ -0,0 +1,10 @@
+// Package ctfsendai2018 is a pseudo customer management system created for the task of CTF Sendai 2018.
+// There are three vulnerabilities in this system, and we exploit the vulnerability from the outside to obtain a hidden flag.
+package ctfsendai2018
+
+// Specification of user information
+// User information has information of name, age, e-mail address, remarks.
+// The name must be between 1 and 20 characters in a string.
+// The age shall be a numerical value of 0 or more.
+// The mail address is a character string and conforms to the mailbox of RFC 5321 and the addr-spec of RFC 5322.
+// Remarks are limited to 50 characters in a character string.
---
+++ users_test.go
@@ -0,0 +1,13 @@
+package ctfsendai2018
+
+// TODO: 氏名にhoge fugaを指定してユーザーのインスタンスが作成されること
+// TODO: 氏名が空のときにインスタンスがnilになり、エラーが返って　こと
+// TODO: 氏名にhoge fugaを指定してNameからhoge fugaが取得できること
+// TODO: 氏名にfoo barを指定してNameからfoo barが取得できること
+// TODO: 年齢に18を指定してAgeから18が取得できること
+// TODO: 年齢に20を指定してAgeから20が取得できること
+// TODO: メールアドレスにhoge@hoge.comを指定してEmailからhoge@hoge.comを取得できること
+// TODO: メールアドレスにfoo@foo.co.jpを指定してEmailからfoo@foo.co.jpを取得できること
+// TODO: メールアドレスにxxxxxxを指定してエラーが取得できること
+// TODO: 備考にxxxxxxを指定してNoteからxxxxxが取得できること
+// TODO: 備考にyyyyyyを指定してNoteからyyyyyが取得できること
