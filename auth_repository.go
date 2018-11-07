fc78158e6136769e8669b139000d9d966843d14f
135yshr <isago@praisefirst.jp>
2018-11-08 08:19:13 +1100

    add testcase of login

--- auth_repository.go
+++ auth_repository.go
@@ -1,7 +1,13 @@
 package ctfsendai2018
 
+// Authentication is structure that represents authentication information
+type Authentication struct {
+	Token string
+}
+
 // AuthRepository is repository manipulating authentication information
 type AuthRepository interface {
+	Login(*User) (*Authentication, error)
 }
 
 // NewAuthRepository is create new instance
@@ -10,3 +16,7 @@ func NewAuthRepository() AuthRepository {
 }
 
 type authRepository struct{}
+
+func (r *authRepository) Login(u *User) (*Authentication, error) {
+	return &Authentication{Token: "eyJleHAiOjE0OTQ1MTQyNjksInVzZXIiOiLnrqHnkIbogIUifQo="}, nil
+}
--- auth_repository_test.go
+++ auth_repository_test.go
@@ -27,3 +27,36 @@ func TestNewAuthRepository(t *testing.T) {
 		})
 	}
 }
+
+func TestLogin(t *testing.T) {
+	type args struct {
+		user *User
+	}
+	tests := map[string]struct {
+		args args
+		want *Authentication
+		err  error
+	}{
+		"アカウントを登録した後登録したユーザーでログインできること": {
+			args: args{
+				user: &User{
+					EMail:    "hoge@hoge.com",
+					Password: "pass",
+				},
+			},
+			want: &Authentication{Token: "eyJleHAiOjE0OTQ1MTQyNjksInVzZXIiOiLnrqHnkIbogIUifQo="},
+		},
+	}
+	for testName, arg := range tests {
+		t.Run(testName, func(t *testing.T) {
+			sut := NewAuthRepository()
+			auth, err := sut.Login(arg.args.user)
+			if err != arg.err {
+				t.Errorf("Error actual: %v, expected: %v", err, arg.err)
+			}
+			if reflect.DeepEqual(auth, arg.want) == false {
+				t.Errorf("Not equals actual: %v, expected: %v", auth, arg.want)
+			}
+		})
+	}
+}
