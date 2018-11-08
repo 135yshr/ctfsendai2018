077a06a12cf9a8842d230007f382bd68745999b7
135yshr <isago@praisefirst.jp>
2018-11-08 18:15:11 +1100

    add testcase of Register

--- auth_usecase.go
+++ auth_usecase.go
@@ -2,13 +2,23 @@ package ctfsendai2018
 
 // AuthUsecase is service manipulating authentication information
 type AuthUsecase interface {
+	Register(string, string) error
 }
 
-// NewAuthService is create new instance
-func NewAuthService(rep AuthRepository) AuthUsecase {
-	return &authService{rep}
+// NewAuthUsecase is create new instance
+func NewAuthUsecase(a AuthRepository, u UserRepository) AuthUsecase {
+	return &authUsecase{a, u}
 }
 
-type authService struct {
-	rep AuthRepository
+type authUsecase struct {
+	authRep AuthRepository
+	userRep UserRepository
+}
+
+func (s *authUsecase) Register(email, passwd string) error {
+	u, err := NewUser(email, passwd)
+	if err != nil {
+		return err
+	}
+	return s.userRep.Add(u)
 }
--- auth_usecase_test.go
+++ auth_usecase_test.go
@@ -6,7 +6,8 @@ import (
 )
 
 func TestNewAuthUsecase(t *testing.T) {
-	rep := NewAuthRepository()
+	authRep := NewAuthRepository()
+	userRep := NewUserRepository()
 	type args struct {
 	}
 	tests := map[string]struct {
@@ -15,16 +16,53 @@ func TestNewAuthUsecase(t *testing.T) {
 		err  error
 	}{
 		"AuthUsecaseのインスタンスが作成できること": {
-			want: &authService{rep},
+			want: &authUsecase{authRep, userRep},
 		},
 	}
 	for testName, arg := range tests {
 		t.Run(testName, func(t *testing.T) {
-			sut := NewAuthService(rep)
+			sut := NewAuthUsecase(authRep, userRep)
 			if reflect.DeepEqual(sut, arg.want) == false {
 				t.Errorf("Not equals actual: %v, expected: %v", sut, arg.want)
 			}
 
+		})
+	}
+}
+
+func TestAuthUsecase_Register(t *testing.T) {
+	type args struct {
+		email    string
+		password string
+	}
+	tests := map[string]struct {
+		args args
+		want []*User
+		err  error
+	}{
+		"メールアドレスhoge@hoge.comで新しくアカウントが登録できること": {
+			args: args{"hoge@hoge.com", "pass"},
+			want: []*User{
+				&User{
+					EMail:    "hoge@hoge.com",
+					Password: "pass",
+					ID:       "aG9nZUBob2dlLmNvbQ",
+					Auth:     2,
+				},
+			},
+		},
+	}
+	for testName, arg := range tests {
+		userRep := NewUserRepository()
+		t.Run(testName, func(t *testing.T) {
+			sut := NewAuthUsecase(NewAuthRepository(), userRep)
+			if err := sut.Register(arg.args.email, arg.args.password); reflect.DeepEqual(err, arg.err) == false {
+				t.Errorf("Error actual: %v, expected: %v", err, arg.err)
+			}
+			list, _ := userRep.List()
+			if reflect.DeepEqual(list, arg.want) == false {
+				t.Errorf("Not equals actual: %v, expected: %v", list, arg.want)
+			}
 		})
 	}
 }
